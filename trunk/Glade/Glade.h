2b0460dc547ef6b8853aaa7a454b40406cfc3285
js <js@drone.ws>
2008-06-09 08:54:52 +0000

    * Create a .ccp file for Glade.h * Lots of code to make it compile

--- trunk/Glade/Glade.h
+++ trunk/Glade/Glade.h
@@ -19,25 +19,38 @@
 
 #include "WConstants.h"
 
-#ifndef GLADE_LINE_H
-#define GLADE_LINE_H
+#include "Audio.h"
 
-// WHEN IN A .cpp FILE I NEED TO ADD THESE LINES (WHY??? I DON'T UNDERSTAND)
-/*#include <stdlib.h>
-#ifndef new
-void * operator new(size_t size)
-{
-  return malloc(size);
-}
-#endif
-#ifndef delete
-void operator delete(void * ptr)
-{
-  free(ptr);
-}
-#endif*/
+#include <string.h>
+#include <avr/pgmspace.h>
+
+#ifndef GLADE_H___
+#define GLADE_H___
+//#ifndef GLADE_H
+//#define GLADE_H
 
 #define BUFFER_SIZE 16
+
+/* Clamps a int32-range int between 0 and 255 inclusive. */
+//#ifndef CLAMP0255
+//unsigned char CLAMP0255(int a)
+//{
+//  return (unsigned char)
+//    ( (((-a) >> 15) & a)  // 0 if the number was negative
+//      | (255 - a) >> 15); // -1 if the number was greater than 255
+//}
+//#endif
+
+/* Provided temporary int t, returns a * b / 255 */
+#define INT_MULT(a,b,t)  ((t) = (a) * (b) + 0x80, ((((t) >> 8) + (t)) >> 8))
+
+/* This version of INT_MULT3 is very fast, but suffers from some
+   slight roundoff errors.  It returns the correct result 99.987
+   percent of the time */
+#define INT_MULT3(a,b,c,t)  ((t) = (a) * (b) * (c) + 0x7F5B, \
+                            ((((t) >> 7) + (t)) >> 16))
+
+#define INT_BLEND(a,b,alpha,tmp)  (INT_MULT((a) - (b), alpha, tmp) + (b))
 
 // Basic class
 ///////////////////////////////////////////
@@ -47,32 +60,13 @@ public:
   GladeNode **inputs;
   byte nInputs;
   
-  GladeNode(byte nInputs = 0) {
-    this->nInputs = nInputs;
-    if (nInputs > 0) {
-      inputs = (GladeNode**)malloc(nInputs*sizeof(GladeNode*));
-      memset(inputs, 0, nInputs);
-    }
-    else
-      inputs = 0;
-  }
+  GladeNode(byte nInputs = 0);  
+  virtual ~GladeNode();
   
-  ~GladeNode() {
-    destroy();
-  }
+  void connect(byte input, GladeNode *node);
+  void destroy();
   
-  void connect(byte input, GladeNode *node) {
-    inputs[(int)input] = node;
-  }
-  
-  virtual void fire() {
-    memset(this->output, 0, BUFFER_SIZE);
-  }
-  
-  void destroy() {
-    free(inputs);
-  }
-  
+  virtual void fire();
 };
 
 
@@ -85,24 +79,10 @@ public:
   int amplitude;
   float _phase;
   
-  GladeSquare(float frequency, int amplitude) : GladeNode(0) {
-    this->frequency = frequency;
-    this->amplitude = amplitude;
-    this->_phase = 0;
-  }
+  GladeSquare(float frequency, int amplitude);
+  ~GladeSquare();
   
-  ~GladeSquare() { GladeNode::destroy(); }
-  
-  void fire() {
-    int i;
-    float num = (float)this->frequency / (float)SAMPLE_RATE;
-    byte* out = this->output;
-    for (i=0; i<BUFFER_SIZE; i++) {
-      *out++ = (this->_phase < 0.5 ? 127 - this->amplitude : 127 + this->amplitude);
-      this->_phase += num;
-      if (this->_phase > 1) this->_phase = 0;
-    }
-  }
+  void fire();
   
 };  
 
@@ -112,32 +92,13 @@ public:
   const prog_uchar *clip;
   int clipLength;
   int clipHead;
+  int offset;
   float speed;
   
-  GladeClip(const prog_uchar *clip, const int clipLength, int clipHead = 0) : GladeNode(0) {
-    this->clip = clip;
-    this->clipLength = clipLength;
-    this->clipHead = clipHead;
-    speed = 1.0f;
-    _phase = 0;
-  }
+  GladeClip(const prog_uchar *clip, const int clipLength, int offset = 0);
+  ~GladeClip();
   
-  ~GladeClip() { 
-    GladeNode::destroy(); 
-  }
-  
-  void fire() {
-    int i;
-    byte *out = output;
-    for (i=0; i<BUFFER_SIZE; i++) {
-      *out++ = pgm_read_byte(&clip[clipHead]);
-      _phase += speed;
-      if (_phase >= 1) {
-        clipHead = (clipHead + (int)_phase) % clipLength;
-        _phase -= (int)_phase;
-      }
-    }
-  }
+  void fire();
 };
 
 
@@ -147,22 +108,10 @@ public:
 class GladeBlend : public GladeNode {
 public:
   byte blend;
-  GladeBlend(byte blend) : GladeNode(2) {
-    this->blend = blend;
-  }
-  ~GladeBlend() { GladeNode::destroy(); }
+  GladeBlend(byte blend);
+  ~GladeBlend();
   
-  void fire() {
-    int i;
-    byte oneMinusBlend = 255 - blend;
-    byte* out = this->output;
-    byte *input1 = inputs[0]->output;
-    byte *input2 = inputs[1]->output;
-    for (i=0; i<BUFFER_SIZE; i++) {
-      *out++ = (byte) ( (((unsigned int)*input1++) * blend + ((unsigned int)*input2++) * oneMinusBlend) / 255 );
-    }
-  }
-  
+  void fire();
 };
 
 class GladeDelay : public GladeNode {
@@ -171,28 +120,10 @@ class GladeDelay : public GladeNode {
   int _currentIndex;
   
 public:
-  GladeDelay(int delayLength) : GladeNode(1) {
-    _record = (byte*) malloc(delayLength * sizeof(byte));
-    _currentIndex = 0;
-    _delayLength = delayLength;
-  }
+  GladeDelay(int delayLength);
+  ~GladeDelay();
   
-  ~GladeDelay() { 
-    free(_record);
-    GladeNode::destroy(); 
-  }
-  
-  void fire() {
-    int i;
-    byte *in = inputs[0]->output;
-    byte *out = output;
-    for (i=0; i<BUFFER_SIZE; i++) {
-      _record[_currentIndex] = *in++;
-      // boaf...
-      *out++ = _record[ (_currentIndex + _delayLength) % _delayLength ];
-      _currentIndex = (++_currentIndex) % _delayLength;
-    }
-  }
+  void fire();
 };
 
 // Sinks
@@ -200,23 +131,14 @@ public:
 
 class GladeOutput : public GladeNode {
 public:
-  GladeOutput() : GladeNode(1) {
-  }
+  GladeOutput();
+  ~GladeOutput();
   
-  ~GladeOutput() { GladeNode::destroy(); }
-  
-  void fire() {
-    int i;
-    byte *in = this->inputs[0]->output;
-//    unsigned long period = 1000000 / SAMPLE_RATE - 7; // subtract 7 us to make up for analogWrite overhead - determined empirically
-    for (i=0; i<BUFFER_SIZE; i++) {
-      Audio.dacWrite(*in++);
-    }
-  }
+  void fire();
   
 };
 
-GladeOutput dac;
+extern GladeOutput dac;
 
 #define Glade_LINE_MAX_NODES 32
 class GladeEngine {
@@ -248,7 +170,6 @@ public:
   }
 };
 
-GladeEngine Glade;
-
+extern GladeEngine Glade;
 
 #endif
