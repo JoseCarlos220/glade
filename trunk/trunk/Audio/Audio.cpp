4f2c408c0c6b4c997137fd22cfc0ad3abe09191f
js@drone.ws <js@drone.ws@6ae136cf-064f-0410-ad3e-8b57523c0284>
2008-06-09 09:01:35 +0000

    Add copyright mention

--- trunk/trunk/Audio/Audio.cpp
+++ trunk/trunk/Audio/Audio.cpp
@@ -1,6 +1,9 @@
 /*
  * This file is part of Glade.
  * 
+ * (c) 2008 Sofian Audry (info@sofianaudry.com)
+ *          Jean-Sébastien Senécal (js@drone.ws)
+ *
  * Glade is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
