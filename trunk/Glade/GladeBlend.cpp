80fb3a70f7914cd11734b900104109986adbcb85
js <js@drone.ws>
2008-07-23 00:14:15 +0000

    Separated nodes in different files (issue #1)

---
+++ trunk/Glade/GladeBlend.cpp
@@ -0,0 +1,39 @@
+/*
+ * This file is part of Glade.
+ * 
+ * (c) 2008 Sofian Audry (info@sofianaudry.com)
+ *          Jean-Sébastien Senécal (js@drone.ws)
+ *
+ * Glade is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Glade is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Glade.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "WConstants.h"
+#include "GladeBlend.h"
+
+GladeBlend::GladeBlend(float blend) : GladeNode(2) {
+  this->blend = (uint8_t) (255 * blend);
+}
+
+GladeBlend::~GladeBlend() { GladeNode::destroy(); }
+
+void GladeBlend::fire() {
+  int i;
+  int tmp;
+  signal* out = this->output;
+  signal *input1 = this->inputs[0]->output;
+  signal *input2 = this->inputs[1]->output;
+  for (i=0; i<BUFFER_SIZE; i++) {
+    *out++ = INT_BLEND(  ((int)(*input1)),  ((int)(*input2)), blend, tmp );
+    input1++; input2++;
+  }
+}
---
+++ trunk/Glade/GladeSquare.cpp
@@ -0,0 +1,41 @@
+/*
+ * This file is part of Glade.
+ * 
+ * (c) 2008 Sofian Audry (info@sofianaudry.com)
+ *          Jean-Sébastien Senécal (js@drone.ws)
+ *
+ * Glade is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Glade is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Glade.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "WConstants.h"
+#include "GladeSquare.h"
+
+GladeSquare::GladeSquare(float frequency, float amplitude, float dutyCycle) : GladeNode(0) {
+  this->frequency = frequency;
+  this->amplitude = (byte) constrain(amplitude * SIGNAL_MAX, 0, SIGNAL_MAX);
+  this->dutyCycle = dutyCycle;
+  this->_phase = 0;
+}
+
+GladeSquare::~GladeSquare() { GladeNode::destroy(); }
+
+void GladeSquare::fire() {
+  int i;
+  float num = (float)this->frequency / (float)Audio.getSampleRate();
+  signal* out = this->output;
+  for (i=0; i<BUFFER_SIZE; i++) {
+    *out++ = (this->_phase < this->dutyCycle ? +this->amplitude : -this->amplitude);
+    this->_phase += num;
+    if (this->_phase > 1) this->_phase = 0;
+  }
+}
